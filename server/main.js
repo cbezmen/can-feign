e9f02786b7d13bf843d3e8af9c6ce42495778720
Can Bezmen <huncan_bezmen@iptiq.com>
2023-02-09 17:16:21 +0100

    initial commit

---
+++ server/main.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end('{\"name\" : \"book 1\"}');
+}).listen(8081);
---
+++ src/main/java/com/feign/client/BookClient.java
@@ -0,0 +1,24 @@
+package com.feign.client;
+
+import com.feign.dto.Book;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@FeignClient(name = "book", contextId = "bookClient")
+public interface BookClient {
+    Logger log = LoggerFactory.getLogger(BookClient.class);
+
+    @CircuitBreaker(name = "bookCB", fallbackMethod = "getBooksFallback")
+    @GetMapping("/")
+    Book getBooks();
+
+    default Book getBooksFallback(Throwable e) {
+        log.error("Here it comes with error", e);
+        return new Book("book FB");
+    }
+
+}
